3deb78e3c9026d5bf5dff99d0f32e7c67ecb2002
MANIKANDAN27 <lmanikandanloganathan@gmail.com>
2016-09-02 12:04:24 +0530

    Create begin_17.c

---
+++ begin_17.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,s=0,t,r;
+printf("enter the integer:");
+scanf("%d",&n);
+t=n;
+while(t!=0)
+{
+r=t%10;
+s=s+r*r*r;
+t=t/10;
+}
+if(n==s)
+printf("given no is armstrong number");
+else
+printf("given no is not armstrong number");
+return 0;
+}
