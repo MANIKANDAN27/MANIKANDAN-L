61f8ca8f41e514043baf66560b2c344fc1bda50a
MANIKANDAN27 <lmanikandanloganathan@gmail.com>
2016-09-03 12:57:40 +0530

    Create begin_1.c

---
+++ begin_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n;
+ printf("enter the number:");
+ scanf(%d",&n);
+ if(n%2==0)
+ printf("given no is even number");
+ else
+ printf("given no is odd number");
+ getch();
+ }
