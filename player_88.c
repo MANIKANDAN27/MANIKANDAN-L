a04b14d2271d5d9c2dce0cceb578725e4cb158d3
MANIKANDAN27 <lmanikandanloganathan@gmail.com>
2016-09-07 10:23:57 +0530

    Create player_88.c

---
+++ player_88.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,c;
+printf("enter the n value:");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+for(c=1;c<i;c++)
+{
+ printf("%d",a);
+ a++;
+ }
+ printf("\n");
+ }
+ getch();
+}
