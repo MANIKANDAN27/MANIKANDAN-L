d70ee99ae6995f48095806d6b97ac50f9cae1c58
MANIKANDAN27 <lmanikandanloganathan@gmail.com>
2016-09-07 10:10:09 +0530

    Create begin_15.c

---
+++ begin_15.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s,e,i;
+printf("enter the start and end interval:");
+scanf("%d%d",&s,&e);
+for(i=s;i<=e;i++)
+{
+ if(i%2==0)
+ printf("%d",i);
+}
+getch();
+}
